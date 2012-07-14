b531521e13df90bfe83fddbb2c2947ac31cf3529
Andrew Madden <bigmoneyhat@gmail.com>
2012-07-14 06:26:10 -0500

    Made TitleState class implement GameState

--- src/org/bigmoneyhat/breakout/gamestate/TitleState.java
+++ src/org/bigmoneyhat/breakout/gamestate/TitleState.java
@@ -1,5 +1,23 @@
 package org.bigmoneyhat.breakout.gamestate;
 
-public class TitleState {
+public class TitleState implements GameState {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
